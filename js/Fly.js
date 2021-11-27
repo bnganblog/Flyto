1d3bc5f720497038160eb94f3c19a687dc64f8ce
bngan <bnganblog@gmail.com>
2021-11-28 00:48:15 +0800

    Create Fly.js

---
+++ js/Fly.js
@@ -0,0 +1,19 @@
+function travelling() {
+    var url = new Array();
+
+  
+    url[0] = "https://github.com/bnganblog/Flyto";
+    url[1] = "https://www.g8d.xyz";
+
+  
+    if (document.referrer) {
+      var origin = new URL(document.referrer).origin;
+      if (url.includes(origin)) {
+        url.splice(url.indexOf(origin), 1);
+      }
+    }
+  
+  
+    var ints = Math.floor(Math.random() * url.length);
+    window.location = url[ints];
+  }
